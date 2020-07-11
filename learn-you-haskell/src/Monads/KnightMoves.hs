435e19085fb909c40a828ba37e0139064370327d
Carlos Gomez <isc.carlos.gomez@gmail.com>
2020-07-11 19:51:09 -0400

    Add more comparisions between Do Notation and Bind (>>=)

--- learn-you-haskell/src/Monads/KnightMoves.hs
+++ learn-you-haskell/src/Monads/KnightMoves.hs
@@ -14,8 +14,37 @@ knightMoves (column, row) = do
     where
         onBoard x = x `elem` [1..8]
 
+-- Implementation using bind (less readable)
+knightMoves' :: KnightPosition -> [KnightPosition]
+knightMoves' (c, r) =
+    pure (c, r)
+    >>= \(c,r) -> [(c + 2, r + 1), (c + 2, r - 1), (c + 1, r + 2), (c + 1, r -2), (c - 2, r + 1), (c - 2, r - 1), (c - 1, r + 2), (c - 1, r -2)]
+    >>= \(c,r) -> guard (c `elem` [1..8] && r `elem` [1..8]) >> return (c,r)
+    where
+        onBoard x = x `elem` [1..8]
+
+-- >>> knightMoves (6, 1)
+-- >>> knightMoves' (6, 1)
+-- [(8,2),(7,3),(4,2),(5,3)]
+-- [(8,2),(7,3),(4,2),(5,3)]
+--
+
+-- Bind is simpler when lambdas are replaced by functions
 in3 :: KnightPosition -> [KnightPosition]
 in3 start = pure start >>= knightMoves >>= knightMoves >>= knightMoves
+
+-- Some times Do Notation is more verbose than bind
+in3' :: KnightPosition -> [KnightPosition]
+in3' start = do
+    start' <- knightMoves start
+    start'' <- knightMoves start'
+    knightMoves start''
+
+-- >>> length $ in3 (6,2)
+-- >>> length $ in3' (6,2)
+-- 202
+-- 202
+--
 
 canReach :: KnightPosition -> KnightPosition -> Bool
 canReach start end = end `elem` in3 start
