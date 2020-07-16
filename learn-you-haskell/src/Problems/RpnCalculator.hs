38cee9651ca9f950f8af63aedfcef3273c889e5d
Carlos Gomez <isc.carlos.gomez@gmail.com>
2020-07-15 22:32:15 -0400

    Improve RpnCalculator to support failure by using the Maybe Monad

--- learn-you-haskell/src/Problems/RpnCalculator.hs
+++ learn-you-haskell/src/Problems/RpnCalculator.hs
@@ -1,23 +1,35 @@
 module RpnCalculator (solveRpn) where
 
--- |
--- A mathematical expression in Reverse Polish Notation.
+import Text.Read
+import Control.Monad
+
+-- | A mathematical expression in Reverse Polish Notation.
 type RpnExpression = String
 
--- |
--- Solves an RPN expression.
-solveRpn :: RpnExpression -> Float
-solveRpn = head . foldl processWord [] . words
-    where
-        processWord (x:y:ys) "+" = (y + x) : ys
-        processWord (x:y:ys) "-" = (y - x) : ys
-        processWord (x:y:ys) "*" = (y * x) : ys
-        processWord (x:y:ys) "/" = (y / x) : ys
-        processWord (x:y:ys) "^" = (y ** x) : ys
-        processWord (x:xs) "ln" = (log x) : xs
-        processWord xs "sum" = [sum xs]
-        processWord xs number = read number : xs
+-- | Solves an RPN expression.
+solveRpn :: RpnExpression -> Maybe Double
+solveRpn expression = do
+    [result] <- foldM processWord [] $ words expression
+    return result
+
+processWord :: [Double] -> String -> Maybe [Double]
+processWord (x:y:ys) "+" = Just $ (y + x) : ys
+processWord (x:y:ys) "-" = Just $ (y - x) : ys
+processWord (x:y:ys) "*" = Just $ (y * x) : ys
+processWord (x:y:ys) "/" = Just $ (y / x) : ys
+processWord (x:y:ys) "^" = Just $ (y ** x) : ys
+processWord (x:xs) "ln" = Just $ (log x) : xs
+processWord xs "sum" = Just $ [sum xs]
+processWord xs number = liftM (:xs) (readMaybe number)
 
 -- >>> solveRpn "10 4 3 + 2 * -"
--- -4.0
+-- >>> solveRpn "1 2 * 4 +"
+-- >>> solveRpn "1 2 * 4 + 5 *"
+-- >>> solveRpn "1 2 * 4"
+-- >>> solveRpn "1 8 wharglbllargh"
+-- Just (-4.0)
+-- Just 6.0
+-- Just 30.0
+-- Nothing
+-- Nothing
 --
